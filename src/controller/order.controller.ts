07c50f779c47bb11ce228278ea8fd9754d7501bc
LamT0305 <Lam45883@gmail.com>
2023-09-28 18:28:09 +0700

    Adding delete function

--- src/controller/order.controller.ts
+++ src/controller/order.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import orderModel from "../model/order.model";
+import { Aggregate } from "mongoose";
 
 export const createOrder = async (req: Request, res: Response) => {
   try {
@@ -53,5 +54,88 @@ export const viewAllOrders = async (req: Request, res: Response) => {
     });
   } catch (error) {
     res.status(500).json({ message: error });
+  }
+};
+
+export const getAllOrdersSortedDate = async (req: Request, res: Response) => {
+  try {
+    const page = parseInt(req.query.page as string) || 1; // Page number, default to 1
+    const perPage = 20; // Items per page, default to 10
+    const sort = parseInt(req.query.sort as string);
+    const skip = (page - 1) * perPage;
+
+    // Create a query to retrieve paginated and sorted data
+    const query = orderModel
+      .find()
+      .skip(skip)
+      .limit(perPage)
+      .sort({ createdAt: sort == 1 ? 1 : -1 }); // -1 : newest first, 1 : oldest first
+
+    // Execute the query to retrieve paginated and sorted orders
+    const sortedOrders = await query.exec();
+
+    // Count the total number of documents matching the query
+    const total = await orderModel.countDocuments();
+
+    res.status(200).json({
+      status: "success",
+      orders: sortedOrders,
+      currentPage: page,
+      totalPages: Math.ceil(total / perPage),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+};
+
+export const getAllOrdersSortedQuantity = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const page = parseInt(req.query.page as string) || 1; // Page number, default to 1
+    const perPage = 20; // Items per page, default to 10
+    const skip = (page - 1) * perPage;
+    const quantity = parseInt(req.query.quantity as string);
+
+    // Create a query to retrieve paginated and sorted data
+    const query = orderModel
+      .find()
+      .skip(skip)
+      .limit(perPage)
+      .sort({ quantity: quantity == -1 ? -1 : 1 }); // -1 : most, 1 : least
+
+    // Execute the query to retrieve paginated and sorted orders
+    const sortedOrders = await query.exec();
+
+    // Count the total number of documents matching the query
+    const total = await orderModel.countDocuments();
+
+    res.status(200).json({
+      status: "success",
+      orders: sortedOrders,
+      currentPage: page,
+      totalPages: Math.ceil(total / perPage),
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+};
+
+export const deleteOrderById = async (req: Request, res: Response) => {
+  try {
+    const isExist = await orderModel.findById(req.params.id);
+    if (!isExist) {
+      res.status(404).json({ status: "error", message: "order not found!" });
+    }
+
+    await orderModel.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json({ status: "success", message: "Order deleted successfully!" });
+  } catch (e) {
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
   }
 };
--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 
-import { createOrder, viewAllOrders } from "../controller/order.controller";
+import {
+  createOrder,
+  viewAllOrders,
+  getAllOrdersSortedDate,
+  getAllOrdersSortedQuantity,
+  deleteOrderById,
+} from "../controller/order.controller";
 
 const router: Router = Router();
 
 router.get("/", viewAllOrders);
 router.post("/", createOrder);
+router.get("/sorted-by-date-newest", getAllOrdersSortedDate);
+router.get("/sorted-by-quantity", getAllOrdersSortedQuantity);
+router.delete("/:id", deleteOrderById);
 
 export default router;
