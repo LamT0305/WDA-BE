9b752dc7efde00860ee782abd6bfe252aba7b46a
LamT0305 <Lam45883@gmail.com>
2023-09-28 11:12:09 +0700

    adding paginate for fetching data

--- src/controller/order.controller.ts
+++ src/controller/order.controller.ts
@@ -27,8 +27,30 @@ export const createOrder = async (req: Request, res: Response) => {
 
 export const viewAllOrders = async (req: Request, res: Response) => {
   try {
-    const orders = await orderModel.find();
-    res.send({ status: "success", orders: orders });
+    const page = req.query.page ? parseInt(req.query.page as string) : 1; // Current page number
+    const pageSize = 20;
+
+    // Calculate the number of documents to skip
+    const skip = (page - 1) * pageSize;
+
+    // Create the aggregation pipeline to retrieve paginated data
+    const pipeline = [{ $skip: skip }, { $limit: pageSize }];
+
+    // Count the total number of documents matching the query
+    const totalDocuments = await orderModel.countDocuments();
+
+    // Calculate the total number of pages
+    const totalPages = Math.ceil(totalDocuments / pageSize);
+
+    // Execute the aggregation query
+    const orders = await orderModel.aggregate(pipeline);
+
+    res.send({
+      status: "success",
+      orders: orders,
+      currentPage: page,
+      totalPages: totalPages,
+    });
   } catch (error) {
     res.status(500).json({ message: error });
   }
